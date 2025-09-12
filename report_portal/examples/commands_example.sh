2e876d754746eec1c0cc2b53e277111c0f860033
Francisco Herrera <fjglira@gmail.com>
2025-09-12 10:47:05 +0200

    Adding starttime to the sample report portal data
    Signed-off-by: Francisco Herrera <fjglira@gmail.com>

--- report_portal/examples/commands_example.sh
+++ report_portal/examples/commands_example.sh
@@ -6,8 +6,8 @@ curl -Ss --fail-with-body --request POST 'https://auth.redhat.com/auth/realms/Em
     | jq -r '.access_token' | jq -R 'split(".") | .[0],.[1] | @base64d | fromjson'
 
 
-droute send --metadata ossm_report_portal.json \
-        --results 'result_example/junit_telemetry_istio.xml' \
+droute send --metadata /Users/frherrer/Documents/repos/playground/report_portal/sample_data/metada_json/istio-release-1.26-integ-ambient_2.json \
+        --results '/Users/frherrer/Documents/repos/playground/report_portal/sample_data/junit_files/istio-release-1.26-integ-ambient_2.xml' \
         --username ossm-oidc-sa \
         --password b71a69cc-880e-432f-8d6e-cd2a4c89d8e4 \
         --url  https://datarouter.ccitredhat.com \
